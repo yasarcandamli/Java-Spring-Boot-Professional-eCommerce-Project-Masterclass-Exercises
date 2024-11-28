2bd5ce40cd8dc900f6ee580bfae6834ea938abbe
yasarcandamli <yasarcandamli@gmail.com>
2024-11-28 23:54:02 +0300

    44. @Qualifier Annotation

--- 01-SpringExample/src/main/java/com/example/autowired/annotation/Manager.java
+++ 01-SpringExample/src/main/java/com/example/autowired/annotation/Manager.java
@@ -1,6 +1,7 @@
 package com.example.autowired.annotation;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -9,7 +10,7 @@ public class Manager {
     private Employee employee;
 
     @Autowired
-    public Manager(Employee employee) {
+    public Manager(@Qualifier("employee") Employee employee) {
         this.employee = employee;
     }
 
