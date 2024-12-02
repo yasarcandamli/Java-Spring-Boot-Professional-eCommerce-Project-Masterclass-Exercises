98eaa7e5db86e84ebdbe2073f8f4659878b88db5
yasarcandamli <yasarcandamli@gmail.com>
2024-12-02 10:29:27 +0300

    59. Making use of @PathVariable

--- 02-FirstSpring/src/main/java/com/example/FirstSpring/HelloController.java
+++ 02-FirstSpring/src/main/java/com/example/FirstSpring/HelloController.java
@@ -1,12 +1,14 @@
 package com.example.FirstSpring;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class HelloController {
+
+    @GetMapping("/hello/{name}/show")
+    public HelloResponse helloParam(@PathVariable String name) {
+        return new HelloResponse("Hello, " + name);
+    }
 
     @GetMapping("/hello")
     public HelloResponse hello() {
