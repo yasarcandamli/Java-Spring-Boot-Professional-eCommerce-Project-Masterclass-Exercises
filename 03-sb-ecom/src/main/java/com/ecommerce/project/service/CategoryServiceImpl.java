16817f53e0c15083749ddf0d757cf9ab0bca66d7
yasarcandamli <yasarcandamli@gmail.com>
2024-12-02 11:48:27 +0300

    66. Delete Category

--- 03-sb-ecom/src/main/java/com/ecommerce/project/service/CategoryServiceImpl.java
+++ 03-sb-ecom/src/main/java/com/ecommerce/project/service/CategoryServiceImpl.java
@@ -7,7 +7,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Service
-public class CategoryServiceImpl implements CategoryService{
+public class CategoryServiceImpl implements CategoryService {
     private List<Category> categories = new ArrayList<>();
     private Long nextId = 1L;
 
@@ -20,5 +20,19 @@ public class CategoryServiceImpl implements CategoryService{
     public void createCategory(Category category) {
         category.setCategoryId(nextId++);
         categories.add(category);
+    }
+
+    @Override
+    public String deleteCategory(Long categoryId) {
+        Category category = categories.stream()
+                .filter(c -> c.getCategoryId().equals(categoryId))
+                .findFirst().orElse(null);
+
+        if (category == null) {
+            return "Category Not Found!";
+        }
+
+        categories.remove(category);
+        return "Category with categoryId: " + categoryId + " deleted successfully!";
     }
 }
