2cb10b4d3aaae2c1534745e51723427778002529
yasarcandamli <yasarcandamli@gmail.com>
2024-12-03 09:48:52 +0300

    69. Challenge: Update Category

--- 03-sb-ecom/src/main/java/com/ecommerce/project/service/CategoryServiceImpl.java
+++ 03-sb-ecom/src/main/java/com/ecommerce/project/service/CategoryServiceImpl.java
@@ -7,6 +7,7 @@ import org.springframework.web.server.ResponseStatusException;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CategoryServiceImpl implements CategoryService {
@@ -37,5 +38,22 @@ public class CategoryServiceImpl implements CategoryService {
 
         categories.remove(category);
         return "Category with categoryId: " + categoryId + " deleted successfully!";
+    }
+
+    @Override
+    public Category updateCategory(Category category, Long categoryId) {
+
+        // ---- It can also be done as in deleteCategory and this is another way ----
+        Optional<Category> optionalCategory = categories.stream()
+                .filter(c -> c.getCategoryId().equals(categoryId))
+                .findFirst();
+
+        if (optionalCategory.isPresent()) {
+            Category existingCategory = optionalCategory.get();
+            existingCategory.setCategoryName(category.getCategoryName());
+            return existingCategory;
+        } else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Category Not Found!");
+        }
     }
 }
