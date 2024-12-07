8cfa943e185f47d82fb674f05ab78c08b77e0a9a
yasarcandamli <yasarcandamli@gmail.com>
2024-12-07 11:11:12 +0300

    116. Updating Response Structure to Include Pagination Details

--- 03-sb-ecom/src/main/java/com/ecommerce/project/service/CategoryServiceImpl.java
+++ 03-sb-ecom/src/main/java/com/ecommerce/project/service/CategoryServiceImpl.java
@@ -41,6 +41,11 @@ public class CategoryServiceImpl implements CategoryService {
 
         CategoryResponse categoryResponse = new CategoryResponse();
         categoryResponse.setContent(categoryDTOS);
+        categoryResponse.setPageNumber(categoryPage.getNumber());
+        categoryResponse.setPageSize(categoryPage.getSize());
+        categoryResponse.setTotalElements(categoryPage.getTotalElements());
+        categoryResponse.setTotalPages(categoryPage.getTotalPages());
+        categoryResponse.setLastPage(categoryPage.isLast());
         return categoryResponse;
     }
 
