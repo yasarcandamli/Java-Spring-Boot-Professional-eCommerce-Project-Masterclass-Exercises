09a6c78dd44cbe83320ab944561de6f35e61323a
yasarcandamli <yasarcandamli@gmail.com>
2024-12-15 23:55:42 +0300

    145. Challenge: Update Product

--- 03-sb-ecom/src/main/java/com/ecommerce/project/service/ProductServiceImpl.java
+++ 03-sb-ecom/src/main/java/com/ecommerce/project/service/ProductServiceImpl.java
@@ -78,4 +78,21 @@ public class ProductServiceImpl implements ProductService{
         productResponse.setContent(productDTOS);
         return productResponse;
     }
+
+    @Override
+    public ProductDTO updateProduct(Long productId, Product product) {
+        Product productFromDb = productRepository.findById(productId)
+                .orElseThrow(() -> new ResourceNotFoundException("Product", "productId", productId));
+
+        productFromDb.setProductName(product.getProductName());
+        productFromDb.setDescription(product.getDescription());
+        productFromDb.setQuantity(product.getQuantity());
+        productFromDb.setPrice(product.getPrice());
+        productFromDb.setDiscount(product.getDiscount());
+        productFromDb.setSpecialPrice(product.getSpecialPrice());
+
+        Product savedProduct = productRepository.save(productFromDb);
+
+        return modelMapper.map(savedProduct, ProductDTO.class);
+    }
 }
