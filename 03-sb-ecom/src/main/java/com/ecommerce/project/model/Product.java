b929ac41711e55cd5abe4f583773c75b023e317a
yasarcandamli <yasarcandamli@gmail.com>
2024-12-15 22:04:13 +0300

    139. Setting Up Product Module

---
+++ 03-sb-ecom/src/main/java/com/ecommerce/project/model/Product.java
@@ -0,0 +1,25 @@
+package com.ecommerce.project.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String productName;
+    private String description;
+    private Integer quantity;
+    private double price;
+    private double specialPrice;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private Category category;
+}
