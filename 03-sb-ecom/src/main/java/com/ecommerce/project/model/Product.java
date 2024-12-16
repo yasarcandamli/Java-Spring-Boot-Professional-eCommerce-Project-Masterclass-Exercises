104b449cef5ce53396aa93a31d983604058fd5ad
yasarcandamli <yasarcandamli@gmail.com>
2024-12-16 19:44:10 +0300

    150. challenge: Adding validations and Custom Exceptions / My customize (Validations deleted from Product and added to ProductDTO)

--- 03-sb-ecom/src/main/java/com/ecommerce/project/model/Product.java
+++ 03-sb-ecom/src/main/java/com/ecommerce/project/model/Product.java
@@ -1,8 +1,6 @@
 package com.ecommerce.project.model;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -15,14 +13,8 @@ public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long productId;
-
-    @NotBlank
-    @Size(min = 3, message = "Product name must contain at least 3 characters! ")
     private String productName;
     private String image;
-
-    @NotBlank
-    @Size(min = 6, message = "Product description must contain at least 6 characters! ")
     private String description;
     private Integer quantity;
     private double price;
