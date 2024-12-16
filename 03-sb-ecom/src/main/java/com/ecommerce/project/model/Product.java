5f86dd5df68ea3c98a263f653dea23f32407ebc6
yasarcandamli <yasarcandamli@gmail.com>
2024-12-16 19:38:37 +0300

    150. challenge: Adding validations and Custom Exceptions

--- 03-sb-ecom/src/main/java/com/ecommerce/project/model/Product.java
+++ 03-sb-ecom/src/main/java/com/ecommerce/project/model/Product.java
@@ -1,6 +1,8 @@
 package com.ecommerce.project.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,9 +14,15 @@ import lombok.NoArgsConstructor;
 public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
+    private Long productId;
+
+    @NotBlank
+    @Size(min = 3, message = "Product name must contain at least 3 characters! ")
     private String productName;
     private String image;
+
+    @NotBlank
+    @Size(min = 6, message = "Product description must contain at least 6 characters! ")
     private String description;
     private Integer quantity;
     private double price;
