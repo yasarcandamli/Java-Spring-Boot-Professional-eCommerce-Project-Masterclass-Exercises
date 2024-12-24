19d36511e40c70dd3e0e19279cf023fa4c471d13
yasarcandamli <yasarcandamli@gmail.com>
2024-12-24 16:48:08 +0300

    189. Creating UserRepository

---
+++ 03-sb-ecom/src/main/java/com/ecommerce/project/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.ecommerce.project.repository;
+
+import com.ecommerce.project.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUserName(String username);
+}
--- 03-sb-ecom/src/main/java/com/ecommerce/project/security/services/UserDetailsServiceImpl.java
+++ 03-sb-ecom/src/main/java/com/ecommerce/project/security/services/UserDetailsServiceImpl.java
@@ -1,5 +1,6 @@
 package com.ecommerce.project.security.services;
 
+import com.ecommerce.project.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -9,7 +10,7 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import com.ecommerce.project.model.User;
-import com.ecommerce.project.repositories.UserRepository;
+import com.ecommerce.project.repository.UserRepository;
 @Service
 public class UserDetailsServiceImpl implements UserDetailsService {
     @Autowired
