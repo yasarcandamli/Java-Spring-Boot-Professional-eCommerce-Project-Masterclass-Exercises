97081a99c2f458ac113d5b394de899ca8eb31c32
yasarcandamli <yasarcandamli@gmail.com>
2024-12-26 21:11:21 +0300

    205. DTOs and Repositories

---
+++ 03-sb-ecom/src/main/java/com/ecommerce/project/repository/CartItemRepository.java
@@ -0,0 +1,9 @@
+package com.ecommerce.project.repository;
+
+import com.ecommerce.project.model.CartItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CartItemRepository extends JpaRepository<CartItem, Long> {
+}
