c76e156f5c8da11d1335e288672e2cb418600008
yasarcandamli <yasarcandamli@gmail.com>
2024-12-27 11:41:09 +0300

    210. Update Product Quantity in the Cart

--- 03-sb-ecom/src/main/java/com/ecommerce/project/service/CartService.java
+++ 03-sb-ecom/src/main/java/com/ecommerce/project/service/CartService.java
@@ -1,6 +1,7 @@
 package com.ecommerce.project.service;
 
 import com.ecommerce.project.payload.CartDTO;
+import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -12,4 +13,7 @@ public interface CartService {
     List<CartDTO> getAllCarts();
 
     CartDTO getCart(String emailId, Long cartId);
+
+    @Transactional
+    CartDTO updateProductQuantityInCart(Long productId, Integer quantity);
 }
