adc4fee135cbb4d19c07586d9a9503d0835da497
yasarcandamli <yasarcandamli@gmail.com>
2024-12-27 10:54:51 +0300

    208. Fetching All Carts

--- 03-sb-ecom/src/main/java/com/ecommerce/project/service/CartService.java
+++ 03-sb-ecom/src/main/java/com/ecommerce/project/service/CartService.java
@@ -3,7 +3,11 @@ package com.ecommerce.project.service;
 import com.ecommerce.project.payload.CartDTO;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public interface CartService {
     CartDTO addProductToCart(Long productId, Integer quantity);
+
+    List<CartDTO> getAllCarts();
 }
