58079bd713564555f4859cbfc1c4eb59993e73d2
yasarcandamli <yasarcandamli@gmail.com>
2025-03-05 17:14:26 +0300

    280. Props and the Spread Syntax

--- 06-React/vite-react-273/src/App.jsx
+++ 06-React/vite-react-273/src/App.jsx
@@ -6,21 +6,27 @@ function App() {
     alert(`You clicked on: ${hobby}`);
   };
 
+  const aliceProfile = {
+    name: 'Alice',
+    age: 23,
+    isMember: true,
+    hobbies: ['Reading', 'Cooking'],
+    onHobbyClick: handleHobbyClick,
+  }
+
+  const johnProfile = {
+    name: 'John',
+    age: 43,
+    isMember: false,
+    hobbies: ['Swimming', 'Hiking'],
+    onHobbyClick: handleHobbyClick,
+  }
+
   return (
     <div className='app-container'>
       <h1>Hello</h1>
-      <ProfileCard
-        name='Alice'
-        age='23'
-        isMember={true}
-        hobbies={['Reading', 'Cooking']}
-        onHobbyClick={handleHobbyClick} />
-      <ProfileCard
-        name='John'
-        age='43'
-        isMember={false}
-        hobbies={['Swimming', 'Hiking']}
-        onHobbyClick={handleHobbyClick} />
+      <ProfileCard {...aliceProfile} />
+      <ProfileCard {...johnProfile} />
     </div>
   )
 }
