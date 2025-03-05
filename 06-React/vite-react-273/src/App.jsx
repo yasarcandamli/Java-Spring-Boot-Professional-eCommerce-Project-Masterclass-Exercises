9801eec655251b1b9320d277993273e81064d525
yasarcandamli <yasarcandamli@gmail.com>
2025-03-05 16:47:36 +0300

    278. Using Props with Arrays

--- 06-React/vite-react-273/src/App.jsx
+++ 06-React/vite-react-273/src/App.jsx
@@ -2,18 +2,11 @@ import './App.css'
 import ProfileCard from './ProfileCard'
 
 function App() {
-  //const person = { name: 'John', age: 30 };
-  //Without destructuring, using Dot notation
-  //const name = person.name;
-  //const age = person.age;
-  //With destructuring
-  //const { name, age } = person;
-
   return (
     <>
       <h1>Hello</h1>
-      <ProfileCard name='Alice' age='23' isMember={true} />
-      <ProfileCard name='John' age='43' isMember={false} />
+      <ProfileCard name='Alice' age='23' isMember={true} hobbies={['Reading', 'Cooking']} />
+      <ProfileCard name='John' age='43' isMember={false} hobbies={['Swimming', 'Hiking']} />
     </>
   )
 }
--- 06-React/vite-react-273/src/ProfileCard.jsx
+++ 06-React/vite-react-273/src/ProfileCard.jsx
@@ -1,11 +1,19 @@
 import PropTypes from 'prop-types';
 
-function ProfileCard({ name, age, isMember }) {
+function ProfileCard({ name, age, isMember, hobbies }) {
     return (
         <div>
             <h3>Name: {name}</h3>
             <h3>Age: {age}</h3>
             <h3>Status: {isMember ? 'Active Member' : 'Guest Member'}</h3>
+            <h3>Hobbies</h3>
+            <ul>
+                {hobbies.map((hobby, index) => {
+                    return (
+                        <li key={index}>{hobby}</li>
+                    );
+                })}
+            </ul>
         </div>
     );
 }
@@ -14,6 +22,7 @@ ProfileCard.propTypes = {
     name: PropTypes.string,
     age: PropTypes.number,
     isMember: PropTypes.bool.isRequired,
+    hobbies: PropTypes.arrayOf(PropTypes.string),
 }
 
 export default ProfileCard;
