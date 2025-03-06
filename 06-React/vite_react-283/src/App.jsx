d6d124704d8285312bdac11303f3970950f363ec
yasarcandamli <yasarcandamli@gmail.com>
2025-03-06 17:25:13 +0300

    284. Managing Multiple State Variables

--- 06-React/vite_react-283/src/App.jsx
+++ 06-React/vite_react-283/src/App.jsx
@@ -3,18 +3,22 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0);
+  const [step, setStep] = useState(1);
 
   const incrementCount = () => {
-    setCount(count + 1)
+    setCount(count + step)
   };
 
   const decrementCount = () => {
-    setCount(count - 1)
+    setCount(count - step)
   };
 
   return (
     <div className='app-container'>
       <h1>Counter Value: {count}</h1>
+      <input type="number"
+        value={step}
+        onChange={(e) => setStep(parseInt(e.target.value))} />
       <button onClick={incrementCount}>Increment</button>
       <button onClick={decrementCount}>Decrement</button>
     </div>
