77d06b36a847779252e0fc6ebca9bb5eeef6e341
yasarcandamli <yasarcandamli@gmail.com>
2025-03-09 13:29:49 +0300

    303. Updating a Custom Hook

--- 06-React/Section-30/302-Example/src/App.jsx
+++ 06-React/Section-30/302-Example/src/App.jsx
@@ -2,13 +2,14 @@ import './App.css'
 import useCounter from './useCounter';
 
 function App() {
-  const { count, increment, decrement } = useCounter();
+  const { count, increment, decrement, reset } = useCounter(10);
 
   return (
     <div>
       <h2>Count: {count}</h2>
       <button onClick={increment}>Increment</button>
       <button onClick={decrement}>Decrement</button>
+      <button onClick={reset}>Reset</button>
     </div>
   )
 }
--- 06-React/Section-30/302-Example/src/useCounter.jsx
+++ 06-React/Section-30/302-Example/src/useCounter.jsx
@@ -1,12 +1,13 @@
 import { useState } from "react";
 
-function useCounter() {
+function useCounter(initialValue = 0) {
     const [count, setCount] = useState(0);
 
     const increment = () => setCount(count + 1);
     const decrement = () => setCount(count - 1);
+    const reset = () => setCount(initialValue);
 
-    return { count, increment, decrement };
+    return { count, increment, decrement, reset };
 }
 
 export default useCounter;
