29d9db164938f8a449942426387e7ca731906705
yasarcandamli <yasarcandamli@gmail.com>
2025-03-10 14:11:03 +0300

    316. Implementing Validations with React Hook Form

--- 06-React/Section-32/312-Example/src/App.jsx
+++ 06-React/Section-32/312-Example/src/App.jsx
@@ -3,7 +3,7 @@ import './App.css'
 import { useForm } from 'react-hook-form';
 
 function App() {
-  const { register, handleSubmit, watch } = useForm();
+  const { register, handleSubmit, watch, formState: { errors } } = useForm();
   const onSubmit = (data) => console.log(data);
 
   // console.log(watch('name'));
@@ -24,12 +24,22 @@ function App() {
       <form onSubmit={handleSubmit(onSubmit)}>
         <label htmlFor="">
           Name:
-          <input {...register('name')} />
+          {/* <input {...register('name', { required: true, minLength: 2 })} /> */}
+          <input {...register('name',
+            {
+              required: 'Name required!',
+              minLength: {
+                value: 2,
+                message: 'Name should be at least 2 characters!'
+              }
+            })} />
+          {errors.name && <p>{errors.name.message}</p>}
         </label>
 
         <label htmlFor="">
           Email:
-          <input {...register('email')} />
+          <input {...register('email', { required: true, pattern: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/ })} />
+          {errors.email && <p>Email is required!</p>}
         </label>
 
         <button type='submit'>Submit</button>
