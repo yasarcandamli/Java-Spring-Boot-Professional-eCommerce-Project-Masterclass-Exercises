2e1995c60831c29561d720bf1fb53f7abd7ba07d
yasarcandamli <yasarcandamli@gmail.com>
2025-03-10 10:06:58 +0300

    305. Handling Loading and Errors

--- 06-React/Section-31/304-Example/src/App.jsx
+++ 06-React/Section-31/304-Example/src/App.jsx
@@ -3,12 +3,32 @@ import './App.css'
 
 function App() {
   const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
+    setLoading(true);
     fetch('https://jsonplaceholder.typicode.com/posts')
       .then(response => response.json())
-      .then(json => setData(json));
+      .then(json => {
+        setData(json);
+        setLoading(false);
+        throw new Error(`Something went wrong!`)
+      })
+      .catch((error) => {
+        console.error(`Error fetching data: `, error);
+        setError(`Failed to fetch the data`);
+        setLoading(false);
+      });
   }, []);
+
+  if (loading) {
+    return <p>Loading...</p>
+  }
+
+  if (error) {
+    return <p>{error}</p>
+  }
 
   return (
     <div>
