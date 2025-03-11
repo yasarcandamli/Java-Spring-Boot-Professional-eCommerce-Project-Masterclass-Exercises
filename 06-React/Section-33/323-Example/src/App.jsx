16a08f339e0b4b0e7099b6f46060c6b77b59c0ac
yasarcandamli <yasarcandamli@gmail.com>
2025-03-11 11:55:20 +0300

    323. Setting Up a React Project with Tailwind CSS

---
+++ 06-React/Section-33/323-Example/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='bg-gray-100 flex justify-center items-center h-screen'>
+      <div className='bg-white p-8 rounded-lg shadow-lg'>
+        <h1 className='text-2xl font-bold text-gray-800'>Hello, Tailwind CSS in REACT!</h1>
+        <p className='text-gray-600'>This is a simple example using Tailwind CSS in React Project!</p>
+      </div>
+    </div>
+  )
+}
+
+export default App
