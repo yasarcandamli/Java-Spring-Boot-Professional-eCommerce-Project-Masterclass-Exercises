85714f4e1c61ef14dc524756dfd8841c69709f83
yasarcandamli <yasarcandamli@gmail.com>
2025-03-13 21:57:10 +0300

    352. Add Product View Functionality (Modal)

--- 07-sb-fs/ecom-frontend/src/components/ProductCard.jsx
+++ 07-sb-fs/ecom-frontend/src/components/ProductCard.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { FaShoppingCart } from 'react-icons/fa';
+import ProductViewModal from './ProductViewModal';
 
 const ProductCard = ({
     productId,
@@ -84,6 +85,12 @@ const ProductCard = ({
                     </button>
                 </div>
             </div>
+            <ProductViewModal
+                open={openProductViewModal}
+                setOpen={setOpenProductViewModal}
+                product={selectedViewProduct}
+                isAvailable={isAvailable}
+            />
         </div>
     )
 }
