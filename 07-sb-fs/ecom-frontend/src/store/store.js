e978dea482f0a3dd53edc464974171c0a36bce5a
yasarcandamli <yasarcandamli@gmail.com>
2025-03-14 12:04:57 +0300

    356. Fetching Products From the API

--- 07-sb-fs/ecom-frontend/src/store/store.js
+++ 07-sb-fs/ecom-frontend/src/store/store.js
@@ -1,8 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { productReducer } from './reducers/ProductReducer';
 
 export const store = configureStore({
-    reducer: {},
-    preloadedState: {},
+    reducer: {
+        products: productReducer
+    },
+    preloadedState: {
+    },
 });
 
 export default store;
