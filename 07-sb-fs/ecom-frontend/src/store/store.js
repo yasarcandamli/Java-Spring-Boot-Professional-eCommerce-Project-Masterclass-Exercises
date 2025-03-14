5473b9369cd811165dbc02ca4fdb2a24baa2e885
yasarcandamli <yasarcandamli@gmail.com>
2025-03-14 14:31:23 +0300

    359. Managing Loaders and Error Scenarios

--- 07-sb-fs/ecom-frontend/src/store/store.js
+++ 07-sb-fs/ecom-frontend/src/store/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { productReducer } from './reducers/ProductReducer';
+import { errorReducer } from './reducers/errorReducer';
 
 export const store = configureStore({
     reducer: {
-        products: productReducer
+        products: productReducer,
+        errors: errorReducer,
     },
     preloadedState: {
     },
