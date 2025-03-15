abe9a35d67eceba0a8fe9c4b70b7ec1c07a72ddc
yasarcandamli <yasarcandamli@gmail.com>
2025-03-15 22:58:02 +0300

    375. Setting up Navbar

--- 07-sb-fs/ecom-frontend/src/App.jsx
+++ 07-sb-fs/ecom-frontend/src/App.jsx
@@ -3,11 +3,13 @@ import './App.css'
 import Products from './components/products/Products'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './components/home/Home'
+import Navbar from './components/shared/Navbar'
 
 
 function App() {
   return (
     <Router>
+      <Navbar />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/products' element={<Products />} />
