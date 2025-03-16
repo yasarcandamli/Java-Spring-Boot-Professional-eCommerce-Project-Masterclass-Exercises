1ea28681b328ffdeb1f43295143734b6db9ea23b
yasarcandamli <yasarcandamli@gmail.com>
2025-03-16 23:23:42 +0300

    380. Building Cart Page || 381. Displaying Cart Content || 382. Allowing Users to Increase Quantity From Cart || 383. Allowing Users to Decrease Quantity From Cart || 384. Allowing Users to Remove Items From Cart || 385. Setting Up Empty Cart Page || 386. Formatting Cart Data to Display || 387. Building a Reactive Cart Badge for the Navbar || 388. Fixing Subtotal Price in Shopping Cart

--- 07-sb-fs/ecom-frontend/src/components/shared/Navbar.jsx
+++ 07-sb-fs/ecom-frontend/src/components/shared/Navbar.jsx
@@ -9,6 +9,7 @@ import { Link, useLocation } from "react-router-dom";
 const Navbar = () => {
     const path = useLocation().pathname;
     const [navbarOpen, setNavbarOpen] = useState(false);
+    const { cart } = useSelector((state) => state.carts);
 
     return (
         <div className="h-[70px] bg-custom-gradient text-white z-50 flex items-center sticky top-0">
@@ -59,7 +60,7 @@ const Navbar = () => {
                             to="/cart">
                             <Badge
                                 showZero
-                                badgeContent={0}
+                                badgeContent={cart?.length || 0}
                                 color="primary"
                                 overlap="circular"
                                 anchorOrigin={{ vertical: 'top', horizontal: 'right', }}>
---
+++ 07-sb-fs/ecom-frontend/src/utils/formatPrice.js
@@ -0,0 +1,11 @@
+export const formatPrice = (amount) => {
+    return new Intl.NumberFormat("en-US", {
+        style: "currency",
+        currency: "USD",
+    }).format(amount);
+}
+
+
+export const formatPriceCalculation = (quantity, price) => {
+    return (Number(quantity) * Number(price)).toFixed(2);
+}
