a95333c115af3eee9ce77789713419034333e25a
yasarcandamli <yasarcandamli@gmail.com>
2025-03-18 10:31:39 +0300

    Section 41: Authenticating Users On Frontend Using React

--- 07-sb-fs/ecom-frontend/src/store/store.js
+++ 07-sb-fs/ecom-frontend/src/store/store.js
@@ -2,12 +2,18 @@ import { configureStore } from '@reduxjs/toolkit';
 import { productReducer } from './reducers/ProductReducer';
 import { errorReducer } from './reducers/errorReducer';
 import { cartReducer } from './reducers/cartReducer';
+import { authReducer } from './reducers/authReducer';
+
+const user = localStorage.getItem("auth")
+    ? JSON.parse(localStorage.getItem("auth"))
+    : null;
 
 const cartItems = localStorage.getItem("cartItems")
     ? JSON.parse(localStorage.getItem("cartItems"))
     : [];
 
 const initialState = {
+    auth: { user: user },
     carts: { cart: cartItems },
 };
 
@@ -16,6 +22,7 @@ export const store = configureStore({
         products: productReducer,
         errors: errorReducer,
         carts: cartReducer,
+        auth: authReducer,
     },
     preloadedState: initialState,
 });
